6096287dd2a7c2ac4e415f7877696240cb76c3c5
ErbolKarmyshakov <erbolkarmysakov37@gmail.com>
2023-10-20 19:24:16 -0500

    Initial Commit

---
+++ Month4 HomeWork1/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Month4 HomeWork1
+//
+//  Created by Mac User on 20/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        view.backgroundColor = UIColor(red: 78/255, green: 156/255, blue: 177/255, alpha: 1)
+        
+    }
+
+
+}
+
