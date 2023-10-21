6e1fc041901a81db90ac08e733d378eb3839617a
ErbolKarmyshakov <erbolkarmysakov37@gmail.com>
2023-10-20 20:14:16 -0500

    add photo

--- Month4 HomeWork1/Controllers/ViewController.swift
+++ Month4 HomeWork1/Controllers/ViewController.swift
@@ -9,13 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var childPhoto: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = UIImage(named: "ChildPhoto")
+        return view
+    }()
+    
+     var viewRed: UIView = {
+        let view = UIView()
+         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.red
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
         view.backgroundColor = UIColor(red: 78/255, green: 156/255, blue: 177/255, alpha: 1)
         
+        view.addSubview(childPhoto)
+        setUpConstraintsForImage()
+        
+       
     }
-
+     func setUpConstraintsForImage(){
+        
+        childPhoto.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 29).isActive = true
+         childPhoto.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        childPhoto.widthAnchor.constraint(equalToConstant: 350).isActive = true
+//        childPhoto.heightAnchor.constraint(equalToConstant: 256).isActive = true
+    }
 
 }
 
